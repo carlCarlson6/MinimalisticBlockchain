142a0f88657a38fa77d446c025a9f651e7d2e28e
Carl <bullet.carlos@gmial.com>
2021-08-15 21:18:29 +0200

    wip

--- src/app/utils/ValidateCanAddBlock.ts
+++ src/app/utils/ValidateCanAddBlock.ts
@@ -1,6 +1,6 @@
 import { Block } from "../../blockchain/block";
 import { PublicKey } from "../../blockchain/publicKey";
 
-export const ValidateCannAddBlock = (block: Block, destinationKey: PublicKey): boolean => {
-    return block.Owner.Key === destinationKey.Key;
+export const ValidateCannAddBlock = (block: Block, inputKey: PublicKey): boolean => {
+    return block.Owner.Key === inputKey.Key;
 }
